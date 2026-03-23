04b502831f07741867a453089133e0db3ec85131
konrad-netizen <30042746@student.swc.ac.uk>
2026-03-23 17:23:44 +0000

    Add files via upload

---
+++ website working prototype/Disclaimer.js
@@ -0,0 +1,3 @@
+
+
+window.alert('Hello this is a diclaimer please exit the site if you dont wish to within the following statments'<p></p>,'The point of this website is to teach and remind people about some cyber security basics information'<p></p>,'This will feature discussion about viruses and hackers, if you wish to avoid it simply close this page' 'At the end of exercise section you will be asked to fill out a short form to help us improve this website');
